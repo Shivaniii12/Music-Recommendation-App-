b8e8f55ecdff494cd7e0a0825d5e0e1757f930eb
Shivani Naik <123252664+Shivaniii12@users.noreply.github.com>
2026-01-15 21:51:26 +0530

    recommend.py

--- src/recommend.py
+++ src/recommend.py
@@ -2,6 +2,8 @@
 import joblib
 import os
 import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Setup logging
 logging.basicConfig(
